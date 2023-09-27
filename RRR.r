3b874d8c3b6d08a5e8e1e34855cd3d81f4829efb
mrtomfen <91942177+mrtomfen@users.noreply.github.com>
2023-09-27 13:19:10 +0200

    merging features of main to init_remote (#1)
    * wanna watch a movie
    
    * snakes are dangerous

--- RRR.r
+++ RRR.r
@@ -1,1 +1,18 @@
-print("no movie here")
+# Create a variable 'time' set to noon (12am)
+time <- as.POSIXct("12:00:00", format = "%H:%M:%S")
+time2 <- as.POSIXct("20:00:00", format = "%H:%M:%S")
+
+# Define the 'rrr' function
+rrr <- function(time) {
+  # Check if 'time' is between 6pm and 10pm
+  if (format(time, "%H:%M:%S") >= "18:00:00" && format(time, "%H:%M:%S") <= "22:00:00") {
+    cat("zacina noc\n")
+  } else {
+    cat("to je moje zena, pekna co\n")
+  }
+}
+
+# Call the 'rrr' function with the 'time' variable as an argument
+rrr(time)
+
+rrr(time2)
